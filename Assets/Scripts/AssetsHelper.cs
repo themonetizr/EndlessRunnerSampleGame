d2bd3870dc600660a2b9ecf71974c213dc9b8607
Elviss Strazdins <elviss@elviss.lv>
2021-10-04 07:24:25 +0300

    Move AssetHelper to Scripts

---
+++ Assets/Scripts/AssetsHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using Monetizr.Challenges;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class AssetsHelper
+{
+    public static IEnumerator DownloadAsset(Challenge.Asset asset, Action<Challenge.Asset, Sprite> onAssetDownloaded)
+    {
+        using UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(asset.url);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(uwr.error);
+            yield break;
+        }
+
+        Texture2D texture = DownloadHandlerTexture.GetContent(uwr);
+        onAssetDownloaded?.Invoke(asset, Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero));
+    }
+}
